25e621672a3e7972c7561efa27263556ab60b8a5
Hemraj-k <83349808+Hemraj-k@users.noreply.github.com>
2022-10-23 06:37:30 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Map from './Map';
+
+const App = () => (
+    <Map />
+);
+
+render(<App />, document.getElementById('root'));
